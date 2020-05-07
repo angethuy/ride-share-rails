ac2e05c1b791c8fa47f226b50b68375bc2807dc2
Jocelyn Wang <wangjoc@gmail.com>
2020-05-06 21:45:33 -0700

    finished driver basic restful routes, except for destroy action

---
+++ app/controllers/drivers_controller.rb
@@ -0,0 +1,86 @@
+class DriversController < ApplicationController
+  def index
+    @drivers = Driver.all
+  end
+
+  def show
+    @driver = Driver.find_by(id: params[:id])
+    if @driver.nil?
+      redirect_to drivers_path
+      return
+    end
+  end
+
+  def new
+    @driver = Driver.new
+  end
+
+  def create
+    @driver = Driver.new(driver_params) 
+
+    if @driver.save #
+      redirect_to drivers_path
+      return
+    else 
+      render :new, status: :bad_request
+      return
+    end
+  end
+
+  def edit
+    @driver = Driver.find_by(id: params[:id])
+
+    if @driver.nil?
+      head :not_found
+      return
+    end
+  end
+
+  def update
+    @driver = Driver.find_by(id: params[:id])
+
+    if @driver.nil?
+      head :not_found
+      return
+    elsif @driver.update(driver_params)
+      redirect_to drivers_path 
+      return
+    else 
+      render :edit, status: :bad_request 
+      return
+    end
+  end
+
+  def destroy
+    driver = Driver.find_by(id: params[:id])
+
+    if driver.nil?
+      head :not_found
+      return
+    end
+
+    driver.destroy
+
+    redirect_to drivers_path
+  end
+
+  def available
+    driver = Driver.find_by(id: params[:id])
+
+    if driver.nil?
+      redirect_to driver_path
+      return
+    end
+
+    driver.available = !driver.available
+    driver.save
+    redirect_to drivers_path
+  end
+
+  private
+
+  def driver_params
+    return params.require(:driver).permit(:name, :vin, :available)
+  end
+
+end
