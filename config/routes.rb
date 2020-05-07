955763b5307a1cd01a2c0aee27c9e44332d5cbd8
Jocelyn Wang <wangjoc@gmail.com>
2020-05-07 00:36:47 -0700

    updated routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,10 +3,12 @@ Rails.application.routes.draw do
   get 'pages/home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :drivers, except: [:destroy] 
-  resources :passengers, :trips
+  resources :passengers, except: [:destroy] 
+  resources :trips, except: [:index]
 
   # Custom Routes
   patch '/drivers/:id/available', to: 'drivers#available', as: 'driver_available'
   patch '/drivers/:id/active', to: 'drivers#active', as: 'driver_active'
   patch '/passengers/:id/active', to: 'passengers#active', as: 'passenger_active'
+  patch '/trips/:id/archive', to: 'trips#archive', as: 'trip_archive'
 end
