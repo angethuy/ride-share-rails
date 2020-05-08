ae2e1dc2132b877c0068a1feff67bde055719196
Angela Nguyen <angethuy@gmail.com>
2020-05-07 21:11:29 -0700

    drivers displayed as table

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,7 +8,11 @@ require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 
+require("packs/custom") 
+
 import "bootstrap"
+
+
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
