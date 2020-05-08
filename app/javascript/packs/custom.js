9e29491e883e87601fc499fea6ba22f525de7b34
Angela Nguyen <angethuy@gmail.com>
2020-05-07 21:45:39 -0700

    driver rows are now clickable

--- app/javascript/packs/custom.js
+++ app/javascript/packs/custom.js
@@ -1,1 +1,9 @@
-console.log("Hello, world! Custom JS loaded.")
+/* re: making a table row clickable with js https://jumpstartrails.com/discussions/50*/
+
+$(document).ready(function(){
+  $(".clickable-tr").on('click', function(e){
+    console.log("clicked a row!");
+    url = $(this).attr('data-path');
+    window.location = url;
+  });
+});
