398f0a54ba1a4419a077b4ca3a9e6281f1439bfb
Jocelyn Wang <wangjoc@gmail.com>
2020-05-10 19:26:12 -0700

    added in data validation to only allow integer from 1-5 for editing rating

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -1,4 +1,7 @@
 class Trip < ApplicationRecord
   belongs_to :driver
   belongs_to :passenger
+
+  # https://stackoverflow.com/questions/24279948/rails-validate-only-on-create-or-on-update-when-field-is-not-blank
+  validates :rating, numericality: { only_integer: true, greater_than: 0, less_than: 6 }, allow_blank: true
 end
