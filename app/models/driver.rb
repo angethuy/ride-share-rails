af568dc1c8e9b4c63c222a236318f5a33b3d200b
Angela Nguyen <angethuy@gmail.com>
2020-05-11 14:30:02 -0700

    refactoring: moved calculations into models

--- app/models/driver.rb
+++ app/models/driver.rb
@@ -2,4 +2,21 @@ class Driver < ApplicationRecord
   has_many :trips
   validates :name, presence: true
   validates :vin, presence: true
+
+  def average_rating
+    return trips.empty? ? 0 : calculate_rating
+  end
+
+  def earnings
+    return trips.map { |trip| trip.rating.nil? ? 0 : (trip.cost - 1.65) * 0.8}.sum 
+  end
+
+  private 
+
+  def calculate_rating
+    ratings_total = trips.map { |trip| trip.rating.nil? ? 0 : trip.rating }.sum
+    return (ratings_total.to_f / trips.length).round(2)
+  end
+
 end
+
--- app/models/passenger.rb
+++ app/models/passenger.rb
@@ -2,4 +2,9 @@ class Passenger < ApplicationRecord
   has_many :trips
   validates :name, presence: true
   validates :phone_num, presence: true
+
+  def total_spent 
+    return trips.map { |trip| trip.rating.nil? ? 0 : trip.cost}.sum 
+  end
+  
 end
